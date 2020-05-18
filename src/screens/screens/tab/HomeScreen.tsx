ed744694fbf74392ab014729c1714b505da1a5bd
unknown <rzc307853639@gmail.com>
2020-05-18 10:17:25 +0800

    1

--- src/screens/screens/tab/HomeScreen.tsx
+++ src/screens/screens/tab/HomeScreen.tsx
@@ -1,24 +1,10 @@
-import React, {useRef} from 'react';
+import React from 'react';
 import {View, Text} from 'react-native';
-import {Modal} from 'beeshell/dist/components/Modal';
-import {connect} from 'react-redux';
-import {MyAppState} from '@/types';
 
-function HomeScreen() {
-  const curRef = useRef(null);
+export default function HomeScreen() {
   return (
     <View>
       <Text>i am HomeScreen </Text>
-      <Modal ref={curRef}>
-        <Text>test</Text>
-      </Modal>
     </View>
   );
 }
-
-const stateToProps = (state: MyAppState) => ({
-  isLoading: state.isLoading,
-  isLogin: state.isLogin,
-});
-
-export default connect(stateToProps)(HomeScreen);
